411db10dd59d79c19349815ee6132b79180d4369
Makotox0207 <xiaoxishicheng@gmail.com>
2021-09-28 23:29:13 +0900

    セッションに含まれる現在のユーザーを検索する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+  # 渡されたuserでログインする
+  def log_in(user)
+    session[:id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
